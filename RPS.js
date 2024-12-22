a5e02fe978f96585e849c5c32a8f77bda923a899
nineikro <nikroesc@gmail.com>
2024-12-22 20:49:50 -0300

    added getHumanChoice

--- RPS.js
+++ RPS.js
@@ -2,12 +2,12 @@ function getComputerChoice(){
     //Math.random(3) doesnt work since it doesnt receive any parameters ... Math.random()
     //Math.floor(x) rounds number to its nearest integer
     //Math.random() gives a number between 0 and 1; multiply that number by 3 and rounding it up you'd have 0 to 2 (3 numbers total)
-    let choice = Math.floor(Math.random() * 3); 
+    let cChoice = Math.floor(Math.random() * 3); 
 
-    if(choice == 0){
+    if(cChoice == 0){
         return "rock"
     } 
-    else if(choice == 1){
+    else if(cChoice == 1){
         return "paper"
     } 
     else {
@@ -15,4 +15,23 @@ function getComputerChoice(){
     }
 }
 
+function getHumanChoice(){
+    //prompt() will make a window popup on browser screen; message is optional
+	let hChoice = prompt("rock, paper or scissors?");
+	
+	if(hChoice.toLowerCase() == "rock"){
+		return "rock";
+	}
+	else if(hChoice.toLowerCase() == "paper"){
+		return "paper";
+	}
+	else if(hChoice.toLowerCase() == "scissors"){
+		return "scissors";
+	}
+    else {
+        return "please try again"
+    }
+}
+
 console.log(getComputerChoice())
+console.log(getHumanChoice())
